46600bf3850fa509506cd7e7dfdf6c956584c0c1
lopez-lab <60268736+lopez-lab@users.noreply.github.com>
2021-02-17 15:57:33 -0500

    Update smiles23D.py

--- smiles23D.py
+++ smiles23D.py
@@ -12,8 +12,8 @@ allinputs=np.genfromtxt(input,dtype='str')
 #...     ...
 
 
-smiles=allinputs[:,1]
-names=allinputs[:,0]
+smiles=allinputs[:,0]
+names=allinputs[:,1]
 for b,c in enumerate(smiles):
 
     mol = AllChem.MolFromSmiles(c)
